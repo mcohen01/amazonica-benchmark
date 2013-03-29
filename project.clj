59a6b2e8f33d9acdc1eeab24779c9d28716f9e1e
Michael Cohen <mcohen01@gmail.com>
2013-03-28 22:19:08 -0700

    add pallet dependencies

--- project.clj
+++ project.clj
@@ -5,5 +5,28 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [amazonica "0.1.2"]
-                 [rotary "0.3.0"]]
+                 [rotary "0.3.0"]
+                 [org.clojure/clojure "1.4.0"]
+                 [org.cloudhoist/pallet "0.7.3"]
+                 [org.cloudhoist/pallet-jclouds "1.4.3"]
+                 [org.jclouds/jclouds-allblobstore "1.4.2"]
+                 [org.jclouds/jclouds-allcompute "1.4.2"]
+                 [org.jclouds.driver/jclouds-slf4j "1.4.2"
+                  :exclusions [org.slf4j/slf4j-api]]
+                 [org.jclouds.driver/jclouds-sshj "1.4.2"]
+                 [ch.qos.logback/logback-classic "1.0.0"]]
+  :dev-dependencies [[org.cloudhoist/pallet
+                      "0.7.3" :type "test-jar"]
+                     [org.cloudhoist/pallet-lein "0.5.2"]]
+  :profiles {:dev
+             {:dependencies
+              [[org.cloudhoist/pallet "0.7.3" :classifier "tests"]]
+              :plugins [[org.cloudhoist/pallet-lein "0.5.2"]]}
+             :leiningen/reply
+             {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.2"]]
+              :exclusions [commons-logging]}}
+  :local-repo-classpath true
+  :repositories
+  {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
+   "sonatype" "https://oss.sonatype.org/content/repositories/releases/"}
   :main benchmark.core)
